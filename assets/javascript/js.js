11352042383c9317ca8e14710fc6ba27c002693f
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2019-10-26 20:20:38 -0700

    gifs are still, can't seem to animate yet

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -2,34 +2,41 @@
 // start of document
 
 // variable to create array for initial buttons
-var giphyButtons = ['The-Rock', 'Ru-Paul', 'Dogs'];
+var giphyButtons = ['The-Rock', 'Ru-Paul', 'SNL', 'Futurama', 'The-Office'];
 
 // dynamically creating buttons
 function renderButtons(){
 
     // start with empty buttons
     $("#button-location").empty();
+
     for (var i = 0; i < giphyButtons.length; i++) {
         // create buttons through array for loop
         var btn = $("<button class='btn btn-primary btn-lg giphy-btn' name=" + giphyButtons[i] + ">");
         btn.text(giphyButtons[i]);
         $("#button-location").append(btn);
     }
-    // when search "add button"
-    $("#button-addon2").on("click", function(event) {
-        event.preventDefault();
-        var searchInput = $("#search-input").val().trim();
-        giphyButtons.push(searchInput);
-        renderButtons();
-    })
 }
 
+// clicking giph buttons to make call to giphy-api
 $(document).on("click", ".giphy-btn", function(){
-    $("#search-input").empty();
-    var btnSelected = $(this).data("name");
+
+    // when user selects "add button"
+    $("#button-addon2").on("click", function(event) {
+        event.preventDefault();
+
+        var searchInput = $("#search-input").val().trim();
+        giphyButtons.push(searchInput);
+
+        $("#search-input").empty();
+        renderButtons();
+    })
+
+    var btnSelected = $(this).attr("name");
     console.log(btnSelected);
+    
     // api call with key
-    var URL = "http;//api.giphy.com/v1/gifs/search?api_key=jELqt1msMihyKzBuTA7k7ATdE1CIiyxi&q=" + btnSelected + "&limit=10&offset=0&rating=PG-13&lang=en";
+    var URL = "https://api.giphy.com/v1/gifs/search?api_key=jELqt1msMihyKzBuTA7k7ATdE1CIiyxi&q=" + btnSelected + "&limit=10&offset=0&rating=PG-13&lang=en";
     
     //ajax api call
     $.ajax({
@@ -39,39 +46,38 @@ $(document).on("click", ".giphy-btn", function(){
         console.log(URL);
         console.log(response);
 
+        var giphs = response.data;
         // creating for loop for response data
-        for (var j = 0; j < response.data.length; j++) {
-            var rating = response.data[i].rating;
+        for (var j = 0; j < giphs.length; j++) {
+            
+            // dynamically building bootstrap card - image half
+            var giphImageElement = 
+                `<div class="card">
+                <img src=${giphs[j].images.original_still.url}
+                class='card-img-top giph' alt='giph' 
+                data-state='still'>`;
 
-            var giphyDiv = $("<div class='giphs-go-here'>");
-            var p = $("<p>").text("Rating " + rating);
-                p.addClass("giphyRating");
-            var animateGiphy = response.data[j].images.fixed_height.url;
-            var stillGiphy = response.data[j].image.fixed_height.url;
-            var giphImage = $("<img>");
-                giphImage.attr("src", "data-still", stillGiphy);
-                //giphImage.attr("data-still", stillGiphy);
-                giphImage.attr("data-animated", "data-state", "still", animateGiphy);
-                //giphImage.attr("data-state", "still");
-                giphyDiv.append(p);
-                giphyDiv.append(giphImage);
-    
-            $("#giph-div").append(giphyDiv);
+                $("#giphs-go-here").append(giphImageElement);
+            
+            var giphTitleRating = "<div class='card-body'>" + "<p class='card-text'>Title: " + giphs[j].title + "</p>" + "<br>" + "<p class='card-text'>Rating: " + giphs[j].rating + "</p>";
+                $("#giphs-go-here").append(giphTitleRating);
+
+            var animateGiph = 
+                `<div class="card">
+                <img src=${giphs[j].images.fixed_height.url}
+                class='card-img-top giph' alt='giph' 
+                data-state='still'>`;
+
+                $(".animate").on("click", function() {
+                    giphImageElement.append(animateGiph);
+                });
+            
+            // btn.text("Animate");
+            // $("giphs-go-here").append(animateBtn);
+            // animate();
         }
-    });
-});
-
-$(document).on("click", ".giphyRating", function(){
-    var giphState = $(this).attr("data-state");
-
-    if (giphState === "still") {
-        $(this).attr("src", $(this).data("animated"));
-        $(this).attr("data-state", "animated");
-    }
-    else {
-        $(this).attr("src", $(this).data("still"));
-        $(this).attr("data-state", "still");
-    }
+    })
+    $("#giphs-go-here").empty();
 })
 
 // calling initial renderButtons function
